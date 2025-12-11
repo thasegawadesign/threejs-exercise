ae4d2edf0ecfe450c43d1c8063b06eeb90eae420
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-11 20:09:27 +0900

    add: group

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -9,27 +9,53 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 // Position
 // mesh.position.x = 0.7;
 // mesh.position.y = -0.6;
 // mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
+// mesh.position.set(0.7, -0.6, 1);
 
 // Scale
 // mesh.scale.x = 2;
 // mesh.scale.y = 0.5;
 // mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+// mesh.scale.set(2, 0.5, 0.5);
 
 // Rotation
-mesh.rotation.reorder('YXZ');
-mesh.rotation.y = Math.PI * 0.25;
-mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.reorder('YXZ');
+// mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.x = Math.PI * 0.25;
 
 // Axes Helper
 const axesHelper = new THREE.AxesHelper();
@@ -51,8 +77,6 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 // camera.position.y = 1;
 camera.position.z = 3;
 scene.add(camera);
-
-camera.lookAt(mesh.position);
 
 /**
  * Renderer
