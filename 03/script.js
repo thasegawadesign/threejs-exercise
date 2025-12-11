6b9f68fefad8545017f3fe7e49e700e1ed3e629f
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-11 17:36:46 +0900

    fix: color

--- 03/script.js
+++ 03/script.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene();
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
+  color: 0xffff00,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
