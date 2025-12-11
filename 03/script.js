8e68215fe314d1de5e8aa3a56843a934dc0bfe58
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-12-11 17:21:43 +0900

    fix: wireframe false

--- 03/script.js
+++ 03/script.js
@@ -11,7 +11,6 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
-  wireframe: true,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
